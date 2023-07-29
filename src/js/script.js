ae3db501ac563333dba4d23665b56daf73e1943f
dav100ide <oreglio.dev@gmail.com>
2023-07-29 11:41:44 +0200

    add git ignore & orbit

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
-'use strict';
+"use strict";
 //import della libreria con namespace
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // render grande quanto la finestra
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,12 +11,24 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const windowAspectRatio = window.innerWidth / window.innerHeight;
-
 const camera = new THREE.PerspectiveCamera(75, windowAspectRatio, 0.1, 1000);
+const orbit = new OrbitControls(camera, renderer.domElement);
 
-const axesHelper = new THREE.AxesHelper(2);
+const axesHelper = new THREE.AxesHelper(8);
 scene.add(axesHelper);
 
-camera.position.z = 5;
-camera.position.y = 2;
-renderer.render(scene, camera);
+camera.position.set(0, 2, 5);
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xeecc00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate(time) {
+   box.rotation.x = time / 1000;
+   box.rotation.y = time / 1000;
+   renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
